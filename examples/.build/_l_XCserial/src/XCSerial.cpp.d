0b931c9591d0ecc4426ebb7d20a375e818d7c7f3
fabriceo <fabrice.oudert@gmail.com>
2020-02-08 09:23:29 +0100

    cleanup

--- examples/.build/_l_XCserial/src/XCSerial.cpp.d
+++
@@ -1,2 +0,0 @@
-.build/_l_XCserial/src/XCSerial.cpp.d .build/_l_XCserial/src/XCSerial.cpp.o .build/_l_XCserial/src/XCSerial.cpp.pca.xml: ../../XMOS/lib_XCserial/src/XCSerial.cpp \
-  ../../XMOS/lib_XCserial/src/XCSerial.h
--- examples/.build/src/examples.xc.d
+++
@@ -1,2 +0,0 @@
-.build/src/examples.xc.d .build/src/examples.xc.o .build/src/examples.xc.pca.xml: ./src/examples.xc
-
